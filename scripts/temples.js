8f4ce54cba973f47a818a9a822836e7be2bd960f
IsaiasPalaco <isaiaspalaco@gmail.com>
2024-10-11 21:21:25 +0200

    finalfinal

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,10 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+});
