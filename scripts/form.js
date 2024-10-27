bb34cbeb93b141db42ac73f7977ca6254e679efb
IsaiasPalaco <isaiaspalaco@gmail.com>
2024-10-27 03:52:57 +0200

    form

---
+++ scripts/form.js
@@ -0,0 +1,34 @@
+const products = [
+    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    const productSelect = document.getElementById('productName');
+    products.forEach(product => {
+        const option = document.createElement('option');
+        option.value = product.name;
+        option.textContent = product.name;
+        productSelect.appendChild(option);
+    });
+});
+
+// Counter for review submissions
+if (localStorage.getItem('reviewCount') === null) {
+    localStorage.setItem('reviewCount', 0);
+}
+
+document.querySelector('form').addEventListener('submit', () => {
+    let reviewCount = parseInt(localStorage.getItem('reviewCount'), 10);
+    localStorage.setItem('reviewCount', reviewCount + 1);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+});
